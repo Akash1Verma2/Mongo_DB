f9c03c129fd168e575a40afd8eb1c670696c3e9a
Akash1Verma2 <ankitverma8621@gmail.com>
2026-03-24 13:37:11 +0530

    Creating Schema and Collection

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 const mongoose = require('mongoose');
 
-mongoose.Connection("mongodb://127.0.0.1:27017/test");
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+}
+
+main().then(() =>{
+    console.log("connection successful");
+})
+    .catch((err) => {
+        console.log(err);
+    })
+
+const userSchema = new mongoose.Schema( {
+    name: String,
+    email: String,
+    age: Number
+})
+
+const User = mongoose.model("User", userSchema);
+const Employee = mongoose.model("Employee", userSchema);
